ad6288510b96f5d3e4e51baf3195c6ae7dea2761
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-12-21 13:47:15 +0400

    OP

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ router.get("/wallets", async (req, res) => {
     .find({}, { projection: { _id: 0, a: 1, b: 1, n: 1 } })
     .skip(0)
     .limit(100)
+    .sort({ _id: -1 })
     .toArray();
   const total = await collection.countDocuments();
   return res.json({ data, total });
@@ -16,9 +17,10 @@ router.get("/wallets", async (req, res) => {
 router.get("/transactions", async (req, res) => {
   const collection = mongoose.connection.db.collection("txns");
   const data = await collection
-    .find({}, { projection: { _id: 0, a: 1, b: 1, n: 1 } })
+    .find({}, { projection: { _id: 0, h: 1, v: 1, f: 1, t: 1, ca: 1 } })
     .skip(0)
     .limit(100)
+    .sort({ _id: -1 })
     .toArray();
   const total = await collection.countDocuments();
   return res.json({ data, total });
@@ -27,9 +29,10 @@ router.get("/transactions", async (req, res) => {
 router.get("/blocks", async (req, res) => {
   const collection = mongoose.connection.db.collection("blocks");
   const data = await collection
-    .find({}, { projection: { _id: 0, a: 1, b: 1, n: 1 } })
+    .find({}, { projection: { _id: 0, nh: 1, bn: 1, ph: 1, tsx: 1, ts: 1 } })
     .skip(0)
     .limit(100)
+    .sort({ _id: -1 })
     .toArray();
   const total = await collection.countDocuments();
   return res.json({ data, total });
