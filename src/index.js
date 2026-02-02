fb1a711cf2f52485dcbd61e36f038beee2521e17
MuhammedJaseel <jaseelmanamulli@gmail.com>
2026-02-02 19:30:51 +0400

    OP

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ await connectDB();
 configRealUpdate();
 
 app.get("", async (req, res) => {
-  return res.json({ app: "Scan API's", status: "WORKING", version: "0.0.1" });
+  return res.json({ app: "Scan API's", status: "WORKING", version: "0.0.2" });
 });
 
 app.use("/", routes);
